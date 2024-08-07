af51d05f223b91a8cd344a90785263cf687b2e09
travistan2003 <164745294+travistan2003@users.noreply.github.com>
2024-08-07 17:48:22 +0800

    Update tasklist.js

--- functions/tasklist.js
+++ functions/tasklist.js
@@ -84,8 +84,14 @@ function scheduleReminder() {
     const time = document.getElementById("time").value.trim();
     const dateTimeString = date + " " + time;
 
+    const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
     if (!title || !date || !time) {
         alert("Please fill in the title, date, and time.");
+        return;
+    }
+
+    if (!dateRegex.test(date)) {
+        alert("Please enter the date in the format DD/MM/YYYY.");
         return;
     }
 
@@ -152,4 +158,3 @@ function scheduleNotification(key, title, information, dateTimeString) {
 }
 
 window.onload = checkUserLogin;
-
